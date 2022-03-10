e7dacb5292b8cc5c2c552eddc63cee99a3c17f1b
felipe <fearlimasi@gmail.com>
2022-03-09 22:55:42 -0300

    init: commit inicial

---
+++ src/main/groovy/io/pismo/app/AppApplication.groovy
@@ -0,0 +1,13 @@
+package io.pismo.app
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AppApplication, args)
+	}
+
+}
