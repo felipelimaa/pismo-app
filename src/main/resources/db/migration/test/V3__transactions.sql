514dfec29992266ba9202d89c0c84eb234074183
felipe <fearlimasi@gmail.com>
2022-03-13 10:05:20 -0300

    test: removed public schema and match args from transactions migration

--- src/main/resources/db/migration/test/V3__transactions.sql
+++ src/main/resources/db/migration/test/V3__transactions.sql
@@ -7,12 +7,8 @@ CREATE TABLE transactions
     eventdate timestamp without time zone,
     CONSTRAINT transactions_pk PRIMARY KEY (transaction_id),
     CONSTRAINT account_fk FOREIGN KEY (account_id)
-            REFERENCES public.accounts (account_id) MATCH SIMPLE
-            ON UPDATE NO ACTION
-            ON DELETE NO ACTION,
+            REFERENCES accounts (account_id),
     CONSTRAINT operationtype_fk FOREIGN KEY (operationtype_id)
-                REFERENCES public.operational_types (operationtype_id) MATCH SIMPLE
-                ON UPDATE NO ACTION
-                ON DELETE NO ACTION
+            REFERENCES operational_types (operationtype_id)
 );
 
